7d52bd8d05ff4b9aac7cea7c78ec1cc21ed236bf
KadManikanta <kadambamanikanta311@gmail.com>
2024-11-22 16:32:31 +0530

    Input added

---
+++ input.cpp
@@ -0,0 +1,72 @@
+
+/*basic skelton
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+
+*/
+
+
+/*instead using all altime  libriary use <bits/stdc++.h>
+
+#include <bits/stdc++.h>
+
+int main(){
+    std::cout<<"reey mowa !";
+    std::cout<<"mani \n";
+    std::cout<<"manikanta";
+
+
+    return 0;
+}
+*/
+
+/*matthematical operations 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<< "manikanta *3";
+    return 0;
+}
+
+*/
+
+/*operations 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 3+3 << "\n";
+    cout << 3*3 << "\n";
+    cout << 10/4 << "\n";
+    return 0;
+}
+
+*/
+
+/* user inputs
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    cout<<"type a number :";
+    cin>>x;
+    cout<<"your number :"<<x;
+    return 0;
+}
+
+*/
+
+
